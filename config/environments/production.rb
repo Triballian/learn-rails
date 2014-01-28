781cbba5336b5fd30a4faef6f127dffb78e8e824
Noe De La Cruz <candiapps@gmail.com>
2014-01-28 22:48:36 +0000

    added r to misspelled config.action_mailer.raise_deliver_erros

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ LearnRails::Application.configure do
   config.action_mailer.default_url_options = { :host => ENV["DOMAIN_NAME"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_erros = false
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
